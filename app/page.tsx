834a538d2b643f90bd5c61e42e75283ad19c5936
Md Faizan <106812942+mdfaizan973@users.noreply.github.com>
2025-12-14 17:46:57 +0530

    Import Services component and update Home layout

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,9 @@
+import Services from "./Services/Services";
+
 export default function Home() {
   return (
-    <div className="flex justify-center items-center">
-      <h1 className="text-primary bg-background text-2xl font-bold">Craftbit</h1>
-    </div>
+    <>
+      <Services />
+    </>
   );
 }
