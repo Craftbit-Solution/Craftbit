84cd3b79b68dc229f07bcdd9516e514af88fc2c9
Md Faizan <106812942+mdfaizan973@users.noreply.github.com>
2025-12-14 17:43:56 +0530

    Add ServiceHero component to Services page

--- app/Services/Services.tsx
+++ app/Services/Services.tsx
@@ -1,1 +1,13 @@
-.
+"use client";
+import ServiceHero from './components/ServiceHero';
+
+export default function Services() {
+    return (
+        <div className="min-h-screen">
+
+            {/* Hero Section */}
+            <ServiceHero />
+
+        </div>
+    );  
+}
