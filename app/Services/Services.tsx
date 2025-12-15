7b8fa47de0b67ac1313b68db321ab556915304ae
Md Faizan <106812942+mdfaizan973@users.noreply.github.com>
2025-12-15 23:02:06 +0530

    Refactor Services component structure and imports

--- app/Services/Services.tsx
+++ app/Services/Services.tsx
@@ -1,21 +1,35 @@
 "use client";
 
+import CTASection from './components/CTASection';
+import IncludedWithProject from './components/IncludedWithProject';
 import ServiceHero from './components/ServiceHero';
-import ServicesOffering  from './components/ServicesOffering';
+import ServicesLists from './components/ServicesLists';
+
+
 
 export default function Services() {
+
+
     return (
-        <div className="min-h-screen">
+        <div className="min-h-screen max-w-7xl mx-auto">
 
             {/* Hero Section */}
             <ServiceHero />
 
-            <div className="w-[70%] mx-auto border-t border-blue-400" />
+            <div className="w-[100%] mx-auto border-t border-grey-400" />
 
             {/* Service Section */}
-            <ServicesOffering />
+            <ServicesLists />
+            
+            <IncludedWithProject />
 
-            <div className="w-[70%] mx-auto border-t border-blue-400" />
+            {/* Our Approach */}
+
+            {/* Lounch Steps */}
+
+            {/* <CTASection /> */}
         </div>
     );
 }
+
+
