1a0d8933bd458fe56ac166d924347558de44a302
Md-Alqma <mdalqma7@gmail.com>
2025-12-21 00:25:30 +0530

    feat: add prettier and eslint config

--- README.md
+++ README.md
@@ -3,18 +3,21 @@
 <!-- Description -->
 
 ## Tech Stack
+
 - Next JS
 - Tailwind CSS
 - Shadcn
 - React motion/GSAP
 
 ## Design Elements
+
 - lucide react
 - font-family
 - color pallete
 - font-size - base/cta/primary/seconday
 
 ## Pages
+
 - Home
 - About
 - Services
--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,9 @@
 export default function Home() {
   return (
-    <div className="flex justify-center items-center">
-      <h1 className="text-primary bg-background text-2xl font-bold">Craftbit</h1>
+    <div className="flex items-center justify-center">
+      <h1 className="text-primary bg-background text-2xl font-bold">
+        Craftbit
+      </h1>
     </div>
   );
 }
