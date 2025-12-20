210e627861d3ae6cf1f44114133cfeeb4919cd98
Md Faizan <106812942+mdfaizan973@users.noreply.github.com>
2025-12-20 21:59:42 +0530

    Import Portfolio component and update Home layout

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,9 @@
+import Portfolio from "./Portfolio/Portfolio";
+
 export default function Home() {
   return (
-    <div className="flex justify-center items-center">
-      <h1 className="text-primary bg-background text-2xl font-bold">Craftbit</h1>
-    </div>
+    <>
+      <Portfolio />
+    </>
   );
 }
