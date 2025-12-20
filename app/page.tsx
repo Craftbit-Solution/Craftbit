a2d1734f7151d9790a09c3e2cabea47a355dda41
Md-Alqma <mdalqma7@gmail.com>
2025-12-21 00:41:06 +0530

    refactor: remove header

--- app/page.tsx
+++ app/page.tsx
@@ -2,11 +2,8 @@ import Services from "./Services/Services";
 
 export default function Home() {
   return (
-    <div className="flex items-center justify-center">
-      <h1 className="text-primary bg-background text-2xl font-bold">
-        Craftbit
-      </h1>
+    <>
       <Services />
-    </div>
+    </>
   );
 }
