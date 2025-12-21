aecaa0950a845659850fa6f5dcfda6c76cf8bd14
Md Faizan <106812942+mdfaizan973@users.noreply.github.com>
2025-12-21 14:19:07 +0530

    Replace content with About component

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,9 @@
+import About from "./About/About";
+
 export default function Home() {
   return (
-    <div className="flex items-center justify-center">
-      <h1 className="text-primary bg-background text-2xl font-bold">
-        Craftbit
-      </h1>
-    </div>
+    <>
+      <About />
+    </>
   );
 }
