d4d20a6876a9d90bb1437d789e97bac4b82a0459
Md Faizan <106812942+mdfaizan973@users.noreply.github.com>
2025-12-21 14:16:28 +0530

    Create About.tsx

---
+++ app/About/About.tsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import OurStory from './components/OurStory'
+import AboutHero from './components/AboutHero'
+import CraftSection from './components/CraftSection'
+import FeaturesSection from './components/FeaturesSection'
+
+export default function About() {
+    return (
+        <div className="min-h-screen max-w-6xl mx-auto">
+            <AboutHero />
+            <OurStory />
+            <CraftSection />
+            <FeaturesSection />
+        </div>
+    )
+}
